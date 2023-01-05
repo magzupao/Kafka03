55dae421b417a56b41f6ce6bce5ac19ef7f31abc
magzupao <mguado@gmail.com>
2023-01-05 19:40:57 +0100

    first commit

---
+++ src/main/java/springboot/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+}
